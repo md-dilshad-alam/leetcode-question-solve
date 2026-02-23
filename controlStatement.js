2b7113c30935ef2d949fa52a2cade3545e0e3ccd
Dilshad Alam <dilshadalam0253@gmail.com>
2026-02-23 16:42:26 +0530

    controls program

---
+++ controlStatement.js
@@ -0,0 +1,13 @@
+
+
+let age = 12;
+
+if (age >= 18){
+  console.log('Eligible to vote ');
+}
+else if (age == 0){
+  console.log('New brown baby');
+}
+else{
+  console.log('Not eligible to vote');
+}
